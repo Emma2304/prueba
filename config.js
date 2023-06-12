86b5622d8094239fd7d4c441c9f66461bd6cb827
Emmanuel <emmanuelgriman@gmail.com>
2023-06-12 16:26:37 -0400

    first commit

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 const PAGE_URL = process.env.NODE_ENV === 'production'
- ? 'placeholder'
- : 'https://todo-list-784i.onrender.com';
+ ? 'https://todo-list-784i.onrender.com'
+ : 'http://localhost:3003';
 
  const MONGO_URI = process.env.NODE_ENV === 'production'
  ? process.env.MONGO_URI_PROD
  : process.env.MONGO_URI_TEST
 
  module.exports = { PAGE_URL, MONGO_URI };
+
+ 
