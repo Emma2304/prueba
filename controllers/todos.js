59665afa5d19d5411adf0a54f1437d0f4fe612d6
Emma2304 <emmanuelgriman@gmail.com>
2023-06-14 13:55:04 -0400

    first commit

--- controllers/todos.js
+++ controllers/todos.js
@@ -28,7 +28,7 @@ todosRouter.delete('/:id', async (request, response) => {
 
     await Todo.findByIdAndDelete(request.params.id);
 
-    user.todos = user.todos.filter(todo => todo.id !== request.params.id);
+    user.todos = user.todos.filter(id => id.toString() !== request.params.id);
     
     await user.save();
     return response.sendStatus(204);
